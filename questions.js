4acf9481bccc712e7bccfb444298dc3ceb38b676
ishwari gangadhar <102672394+csTAE@users.noreply.github.com>
2023-02-15 22:28:24 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "How long is the first Harry Potter movie: Harry Potter and The Philosopher's Stone?",
+    choice1: "1.5 hours",
+    choice2: "3.5 hours",
+    choice3: "2.5 hours",
+    choice4: "4.5 hours",
+    answer: 2
+},
+            {
+            question: "Which 1984 film featured a main character named Axel Foley?",
+    choice1: "Back To The Future",
+    choice2: "Footloose",
+    choice3: "Beverley Hills Cop",
+    choice4: "The Breakfast Club",
+    answer: 2
+},
+            {
+            question: "What is Dorothy's surname in The Wizard of Oz?",
+    choice1: "Bale",
+    choice2: "Hale",
+    choice3: "Gale",
+    choice4: "Yale",
+    answer: 2
+},
+            {
+            question: "Who perfroms the most on-screen kills in the Lord of The Rings movies?",
+    choice1: "Boromir",
+    choice2: "Gimli",
+    choice3: "Legolas",
+    choice4: "Frodo",
+    answer: 2
+},
+            {
+            question: "How many Oscars did the 1994 movie Forrest Gump win?",
+    choice1: "3 oscars",
+    choice2: "9 oscars",
+    choice3: "6 oscars",
+    choice4: "12 oscars",
+    answer: 2
+},
+            {
+            question: "In which form did 'The Jungle Book' originally appear?In which form did 'The Jungle Book' originally appear?",
+    choice1: "Radio Play",
+    choice2: "Film",
+    choice3: "Book",
+    choice4: "An app",
+    answer: 2
+},
+            {
+               question: "Which city is the setting for the 1950s film musical Gigi?",
+    choice1: "London",
+    choice2: "Paris",
+    choice3: "New York",
+    choice4: "Rio de Janeiro",
+    answer: 1
+},{
+               question: "What world-famous movie monster turned 50 in 2005?",
+    choice1: "Frankenstein",
+    choice2: "King Kong",
+    choice3: "Mothra",
+    choice4: "Godzilla",
+    answer: 3
+},
+            {
+               question: "Where were The Lord of the Rings movies filmed?",
+    choice1: "Ireland",
+    choice2: "Iceland",
+    choice3: "New Zealand",
+    choice4: "Australia",
+    answer: 2
+},
+            {
+               question: "Who did the cat in The Godfather belong to?",
+    choice1: "Diane Keaton",
+    choice2: "No one—the cat was a stray",
+    choice3: "Al Pachino",
+    choice4: "Francis Ford Coppola",
+    answer: 1
+}];
